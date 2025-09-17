e8636acaa93c5d736266642c373e9818ac147a6b
pauldeogratias <pauldeogratias12@gmail.com>
2025-09-17 15:51:21 +0300

    Update files

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// # 9. TESTING SETUP
+
+// # Jest configuration
+// # jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapping: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+module.exports = createJestConfig(customJestConfig)
