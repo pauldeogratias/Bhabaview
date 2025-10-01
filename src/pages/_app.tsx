5e6d0ff1351d96f2796f03b41acac70a0b39a0a4
pauldeogratias <pauldeogratias12@gmail.com>
2025-10-01 14:58:12 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -528,6 +528,65 @@ function MyApp({ Component, pageProps }: AppProps<ExtendedPageProps>) {
           `
         }}
       />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+      {/* Facebook Meta Pixel - only run in production */}
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            id="facebook-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1750437865634260');
+                fbq('track', 'PageView');
+              `
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1750437865634260&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+        </>
+      )}
+
+
+
+
+
+
+
+
+
+
+      
       
       {/* Performance monitoring */}
       {process.env.NODE_ENV === 'development' && <PerformanceMonitor />}
@@ -583,5 +642,6 @@ function MyApp({ Component, pageProps }: AppProps<ExtendedPageProps>) {
     </>
   )
 }
+
 
 export default MyApp
