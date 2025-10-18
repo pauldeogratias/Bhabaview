6377b0d26f1cb37fe6549f97a5b03d6f68170f4c
pauldeogratias <pauldeogratias12@gmail.com>
2025-10-18 20:19:37 +0300

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,14 +1,6 @@
 // global.d.ts
-
-export {};
-
-declare global {
-  interface Window {
-    fbq?: (...args: any[]) => void;
-  }
-}
- 
 interface Window {
   gtag?: (...args: (string | number | Record<string, unknown>)[]) => void
 }
+
 
